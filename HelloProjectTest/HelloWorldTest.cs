5d7a5b48a18f8bf9739cdc721b7d2456705eb80e
Javed Ahmad <mailtojavedahmad@gmail.com>
2019-02-06 18:19:04 +0530

    Added More test

--- HelloProjectTest/HelloWorldTest.cs
+++ HelloProjectTest/HelloWorldTest.cs
@@ -20,5 +20,20 @@ namespace HelloProjectTest
             //Assert
             Assert.AreEqual(expeted, actual);
         }
+
+        [TestMethod]
+        public void Test_DivideMethodWithNegative()
+        {
+            // Arrange
+            int expeted = 2;
+            int numerator = 10;
+            int denominator = 5;
+
+            //Act
+            int actual = HelloWorldProject.Program.DivideMethod(numerator, denominator);
+
+            //Assert
+            Assert.AreEqual(expeted, actual);
+        }
     }
 }
